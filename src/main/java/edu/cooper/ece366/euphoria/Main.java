5c3f8e7672447b1b5bbba1bfd778dc47e0adbaf9
George Ho <19851673+eigenfoo@users.noreply.github.com>
2019-03-16 11:49:53 -0400

    BUG: helloworld compiles

--- src/main/java/edu/cooper/ece366/euphoria/Main.java
+++ src/main/java/edu/cooper/ece366/euphoria/Main.java
@@ -1,4 +1,6 @@
-public class HelloWorld {
+package edu.cooper.ece366.euphoria;
+
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World");
     }
