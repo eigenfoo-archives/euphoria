26842f41a9d547fbd097a438af4e1e5a63800edc
George Ho <19851673+eigenfoo@users.noreply.github.com>
2019-03-16 12:28:50 -0400

    BLD: build main from ethans maint

--- euphoria-service/src/main/java/edu/cooper/ece366/euphoria/Main.java
+++ euphoria-service/src/main/java/edu/cooper/ece366/euphoria/Main.java
@@ -1,7 +1,24 @@
 package edu.cooper.ece366.euphoria;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.spotify.apollo.Environment;
+import com.spotify.apollo.httpservice.HttpService;
+import com.spotify.apollo.httpservice.LoadingException;
+import com.spotify.apollo.route.Route;
+import io.norberg.automatter.jackson.AutoMatterModule;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World");
+    public static void main(String[] args) throws LoadingException {
+        HttpService.boot(Main::init, "euphoria-service", args);
+    }
+
+    private static void init(final Environment environment) {
+        ObjectMapper objectMapper = new ObjectMapper().registerModule(new AutoMatterModule());
+        PostingHandlers postingHandlers = new PostingHandlers(objectMapper);
+
+        environment
+                .routingEngine()
+                .registerAutoRoute(Route.sync("GET", "/ping", rc -> "pong"))
+                .registerRoutes(postingHandlers.routes());
     }
 }
