577928d46f3d5d4679515c2265b45f0c9c185b53
George Ho <19851673+eigenfoo@users.noreply.github.com>
2019-03-16 16:15:45 -0400

    MAINT: fix postinghandles

--- euphoria-service/src/main/java/edu/cooper/ece366/euphoria/PostingHandles.java
+++ euphoria-service/src/main/java/edu/cooper/ece366/euphoria/PostingHandles.java
@@ -17,6 +17,10 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import okio.ByteString;
 
+import static edu.cooper.ece366.euphoria.Industry.HEALTHCARE;
+import static edu.cooper.ece366.euphoria.Location.NEWYORK;
+import static edu.cooper.ece366.euphoria.SkillLevel.INTERNSHIP;
+
 public class PostingHandles {
 
     private final ObjectMapper objectMapper;
@@ -32,7 +36,14 @@ public class PostingHandles {
     }
 
     private List<edu.cooper.ece366.euphoria.Posting> getPostings(final RequestContext requestContext) {
-        edu.cooper.ece366.euphoria.Posting posting = new PostingBuilder().postingId(31415).jobTitle("Underwater Basket Weaver").description("What it sounds like.").build();
+        edu.cooper.ece366.euphoria.Posting posting = new PostingBuilder()
+                .postingId(31415)
+                .jobTitle("Underwater Basket Weaver")
+                .description("What it sounds like.")
+                .location(NEWYORK)
+                .skillLevel(INTERNSHIP)
+                .industry(HEALTHCARE)
+                .build();
         return Collections.singletonList(posting);
     }
 
