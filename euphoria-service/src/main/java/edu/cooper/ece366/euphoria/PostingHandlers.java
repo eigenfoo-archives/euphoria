ea0248a6c46d2417f44b0097a98b8a89da76b730
George Ho <19851673+eigenfoo@users.noreply.github.com>
2019-03-18 13:45:38 -0400

    ENH: query database in posting handlers (#4)
    * MAINT: jdbc does not support enums; store in db as strings
    
    * MAINT: convert enums to strings in db
    
    * BLD: building getPosting function
    
    * BLD: first pass done
    
    * BUG: fix typo
    
    * BLD: getPosting works now
    
    * STY: reformat code
    
    * BLD: createPosting now works
    
    * STY: reformat code
    
    * MAINT: touch up db script
    
    * STY: call them handlers!
    
    * STY: rename
    
    * MAINT: rename to PostingHandlers
    
    * BUG: fix typo

---
+++ euphoria-service/src/main/java/edu/cooper/ece366/euphoria/PostingHandlers.java
@@ -0,0 +1,98 @@
+package edu.cooper.ece366.euphoria;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.spotify.apollo.RequestContext;
+import com.spotify.apollo.Response;
+import com.spotify.apollo.route.*;
+import okio.ByteString;
+
+import java.sql.*;
+import java.time.ZoneId;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class PostingHandlers implements RouteProvider {
+    private static final String dbUrl = "jdbc:mysql://localhost:3306/euphoria";
+    private static final String dbUsername = "euphoria";
+    private static final String dbPassword = "euphoria";
+    private final ObjectMapper objectMapper;
+
+    public PostingHandlers(final ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+    @Override
+    public Stream<Route<AsyncHandler<Response<ByteString>>>> routes() {
+        return Stream.of(
+                Route.sync("GET", "/posting/<postingId>", this::getPosting),
+                Route.sync("POST",
+                        "/posting/<companyId>/<jobTitle>/<description>/<location>/<skillLevel>/<industry>",
+                        this::createPosting)
+        ).map(r -> r.withMiddleware(jsonMiddleware()));
+    }
+
+    private List<Posting> getPosting(final RequestContext rc) {
+        Posting posting = null;
+
+        try {
+            Integer postingId = Integer.valueOf(rc.pathArgs().get("postingId"));
+            Connection conn = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
+            String sqlQuery = "SELECT * FROM postings WHERE postingId = ?";
+            PreparedStatement ps = conn.prepareStatement(sqlQuery);
+            ps.setInt(1, postingId);
+            ResultSet resultSet = ps.executeQuery();
+
+            if (resultSet.next()) {  //FIXME Only read the first result. There should only be one, after all...
+                posting = new PostingBuilder()
+                        .postingId(resultSet.getInt("postingId"))
+                        .jobTitle(resultSet.getString("jobTitle"))
+                        .description(resultSet.getString("description"))
+                        .location(Location.valueOf(resultSet.getString("location")))
+                        .skillLevel(SkillLevel.valueOf(resultSet.getString("skillLevel")))
+                        .industry(Industry.valueOf(resultSet.getString("industry")))
+                        .build();
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+
+        return Collections.singletonList(posting);
+    }
+
+    private List<Posting> createPosting(final RequestContext rc) {
+        try {
+            Integer companyId = Integer.valueOf(rc.pathArgs().get("companyId"));
+            String jobTitle = rc.pathArgs().get("jobTitle");
+            String description = rc.pathArgs().get("description");
+            Location location = Location.valueOf(rc.pathArgs().get("location"));
+            SkillLevel skillLevel = SkillLevel.valueOf(rc.pathArgs().get("skillLevel"));
+            Industry industry = Industry.valueOf(rc.pathArgs().get("industry"));
+
+            Connection conn = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
+            String sqlQuery = "INSERT INTO postings (companyId, jobTitle, " +
+                    "description, location, industry, skillLevel, " +
+                    "dateCreated) VALUES (?, ?, ?, ?, ?, ?, ?)";
+            PreparedStatement ps = conn.prepareStatement(sqlQuery);
+            ps.setInt(1, companyId);
+            ps.setString(2, jobTitle);
+            ps.setString(3, description);
+            ps.setString(4, location.toString());
+            ps.setString(5, skillLevel.toString());
+            ps.setString(6, industry.toString());
+            Date date = new Date();
+            ps.setObject(7, date.toInstant().atZone(ZoneId.of("UTC")).toLocalDate());
+            ps.executeUpdate();
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+
+        return Collections.emptyList();
+    }
+
+    private <T> Middleware<AsyncHandler<T>, AsyncHandler<Response<ByteString>>> jsonMiddleware() {
+        return JsonSerializerMiddlewares.<T>jsonSerialize(objectMapper.writer())
+                .and(Middlewares::httpPayloadSemantics);
+    }
+}
--- euphoria-service/src/main/resources/initialize-database.sql
+++ euphoria-service/src/main/resources/initialize-database.sql
@@ -1,75 +1,90 @@
 -- Initializes database and tables.
--- To run: `cat create-database.sql | mysql -u root -p`
+-- To run: `cat initialize-database.sql | mysql -u root -p`
 
-drop database if exists euphoria;
+DROP DATABASE IF EXISTS euphoria;
 
-drop user if exists 'euphoria'@'localhost';
+DROP USER IF EXISTS 'euphoria'@'localhost';
 
-create database euphoria;
+CREATE DATABASE euphoria;
 
-create user 'euphoria'@'localhost' identified by 'euphoria';
+USE euphoria;
 
-use euphoria;
+SET GLOBAL time_zone = '+0:00';
 
-create table users (userId INT AUTO_INCREMENT PRIMARY KEY,
+CREATE USER 'euphoria'@'localhost' IDENTIFIED WITH mysql_native_password BY 'euphoria';
+
+GRANT ALL PRIVILEGES ON euphoria.* TO 'euphoria'@'localhost';
+
+-- educationLevel is one of: "NOHIGHSCHOOL",
+--                           "HIGHSCHOOL",
+--                           "GED",
+--                           "SOMECOLLEGE",
+--                           "ASSOCIATES",
+--                           "BACHELORS",
+--                           "MASTERS",
+--                           "PHD",
+--                           "MD",
+--                           "JD"
+CREATE TABLE users (userId INT AUTO_INCREMENT PRIMARY KEY,
                     name VARCHAR(40) NOT NULL,
                     email VARCHAR (40) NOT NULL,
                     phoneNumber VARCHAR(20) NOT NULL,
-                    educationLevel ENUM ("NOHIGHSCHOOL",
-                                         "HIGHSCHOOL",
-                                         "GED",
-                                         "SOMECOLLEGE",
-                                         "ASSOCIATES",
-                                         "BACHELORS",
-                                         "MASTERS",
-                                         "PHD",
-                                         "MD",
-                                         "JD") NOT NULL,
+                    educationLevel VARCHAR(20) NOT NULL,
                     description TEXT NOT NULL,
                     dateCreated DATETIME NOT NULL); 
 
-create table companies (companyId INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE companies (companyId INT AUTO_INCREMENT PRIMARY KEY,
                         name VARCHAR(40) NOT NULL,
                         website VARCHAR (40) NOT NULL,
                         description TEXT NOT NULL,
                         dateCreated DATETIME NOT NULL);
 
-create table postings (postingId INT AUTO_INCREMENT PRIMARY KEY,
-                       companyId INT NOT NULL, FOREIGN KEY(companyId) REFERENCES companies (companyId),
+-- location is one of "NEWYORK",
+--                    "LONDON",
+--                    "HONGKONG",
+--                    "BERLIN",
+--                    "BEIJING",
+--                    "WASHINGTON"
+-- industry is one of "EDUCATION",
+--                    "ENERGY",
+--                    "FINANCE",
+--                    "FOOD",
+--                    "HEALTHCARE",
+--                    "INSURANCE",
+--                    "MEDIA",
+--                    "RETAIL",
+--                    "SERVICES",
+--                    "TECHNOLOGY",
+--                    "TRANSPORT",
+--                    "UTILITIES"
+-- skillLevel is one of "INTERNSHIP",
+--                      "ENTRYLEVEL",
+--                      "ASSOCIATE",
+--                      "SENIOR",
+--                      "DIRECTOR",
+--                      "EXECUTIVE"
+CREATE TABLE postings (postingId INT AUTO_INCREMENT PRIMARY KEY,
+                       companyId INT NOT NULL,  -- FOREIGN KEY(companyId) REFERENCES companies (companyId),
                        jobTitle VARCHAR (30) NOT NULL,
                        description TEXT NOT NULL,
-                       location ENUM ("NEWYORK",
-                                      "LONDON",
-                                      "HONGKONG",
-                                      "BERLIN",
-                                      "BEIJING",
-                                      "WASHINGTON") NOT NULL,
-                       industry ENUM ("EDUCATION",
-                                      "ENERGY",
-                                      "FINANCE",
-                                      "FOOD",
-                                      "HEALTHCARE",
-                                      "INSURANCE",
-                                      "MEDIA",
-                                      "RETAIL",
-                                      "SERVICES",
-                                      "TECHNOLOGY",
-                                      "TRANSPORT",
-                                      "UTILITIES") NOT NULL ,
-                       skillLevel ENUM ("INTERNSHIP",
-                                        "ENTRYLEVEL",
-                                        "ASSOCIATE",
-                                        "SENIOR",
-                                        "DIRECTOR",
-                                        "EXECUTIVE") NOT NULL,
+                       location VARCHAR(20) NOT NULL,
+                       industry VARCHAR(20) NOT NULL,
+                       skillLevel VARCHAR(20) NOT NULL,
                        dateCreated DATETIME NOT NULL);
 
-create table applications (applicationId INT AUTO_INCREMENT PRIMARY KEY,
-                           postingId INT NOT NULL, FOREIGN KEY(postingId) REFERENCES postings (postingId),
-                           userId INT NOT NULL, FOREIGN KEY(userId) REFERENCES users (userID), 
+INSERT INTO postings
+    (companyId, jobTitle, description, location, industry, skillLevel, dateCreated)
+VALUES
+    (123, 'Underwater Basket Weaver', 'Must lift.', 'WASHINGTON', 'SERVICES', 'INTERNSHIP', "2018-07-10 02:30:00"),
+    (456, 'Frontend Developer', 'Must know everything about React.js.', 'NEWYORK', 'TECHNOLOGY', 'EXECUTIVE', "2019-02-12 12:00:00"),
+    (789, 'Backend Developer', 'Must know nothing about React.js.', 'NEWYORK', 'TECHNOLOGY', 'INTERNSHIP', "2019-03-16 23:59:59");
+
+CREATE TABLE applications (applicationId INT AUTO_INCREMENT PRIMARY KEY,
+                           postingId INT NOT NULL,  -- FOREIGN KEY(postingId), REFERENCES postings (postingId),
+                           userId INT NOT NULL, -- FOREIGN KEY(userId), REFERENCES users (userID), 
                            resume TEXT NOT NULL,
                            coverLetter TEXT NOT NULL);
 
-create table authentication (username VARCHAR(30) NOT NULL,
+CREATE TABLE authentication (username VARCHAR(30) NOT NULL,
                              passwordHash VARCHAR(40) NOT NULL,
-                             userbool BOOLEAN NOT NULL);
+                             isUser BOOLEAN NOT NULL);
