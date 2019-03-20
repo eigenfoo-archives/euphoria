7944a126e8735778fe49e2867083d63d39d81641
George Ho <19851673+eigenfoo@users.noreply.github.com>
2019-03-20 11:04:31 -0400

    MAINT: add examples to user db

--- euphoria-service/src/main/resources/initialize-database.sql
+++ euphoria-service/src/main/resources/initialize-database.sql
@@ -3,17 +3,17 @@
 
 DROP DATABASE IF EXISTS euphoria;
 
-DROP USER IF EXISTS 'euphoria'@'localhost';
+DROP USER IF EXISTS "euphoria"@"localhost";
 
 CREATE DATABASE euphoria;
 
 USE euphoria;
 
-SET GLOBAL time_zone = '+0:00';
+SET GLOBAL time_zone = "+0:00";
 
-CREATE USER 'euphoria'@'localhost' IDENTIFIED WITH mysql_native_password BY 'euphoria';
+CREATE USER "euphoria"@"localhost" IDENTIFIED WITH mysql_native_password BY "euphoria";
 
-GRANT ALL PRIVILEGES ON euphoria.* TO 'euphoria'@'localhost';
+GRANT ALL PRIVILEGES ON euphoria.* TO "euphoria"@"localhost";
 
 -- educationLevel is one of: "NOHIGHSCHOOL",
 --                           "HIGHSCHOOL",
@@ -32,6 +32,13 @@ CREATE TABLE users (userId INT AUTO_INCREMENT PRIMARY KEY,
                     educationLevel VARCHAR(20) NOT NULL,
                     description TEXT NOT NULL,
                     dateCreated DATETIME NOT NULL); 
+
+INSERT INTO users
+    (name, email, phoneNumber, educationLevel, description, dateCreated)
+VALUES
+    ("Johnny Appleseed", "john@appleseed.com", "123-456-7890", "JD", "I like Macintoshes.", "2017-03-06 15:20:00"),
+    ("Tim Apple", "tim@apple.com", "456-123-7890", "MASTERS", "I also like Macintoshes.", "2018-02-10 18:45:00"),
+    ("Jeff Bozo", "jeff@bozo.com", "890-123-4567", "BACHELORS", "I will not work in Queens.", "2019-08-01 01:00:00");
 
 CREATE TABLE companies (companyId INT AUTO_INCREMENT PRIMARY KEY,
                         name VARCHAR(40) NOT NULL,
@@ -75,9 +82,9 @@ CREATE TABLE postings (postingId INT AUTO_INCREMENT PRIMARY KEY,
 INSERT INTO postings
     (companyId, jobTitle, description, location, industry, skillLevel, dateCreated)
 VALUES
-    (123, 'Underwater Basket Weaver', 'Must lift.', 'WASHINGTON', 'SERVICES', 'INTERNSHIP', "2018-07-10 02:30:00"),
-    (456, 'Frontend Developer', 'Must know everything about React.js.', 'NEWYORK', 'TECHNOLOGY', 'EXECUTIVE', "2019-02-12 12:00:00"),
-    (789, 'Backend Developer', 'Must know nothing about React.js.', 'NEWYORK', 'TECHNOLOGY', 'INTERNSHIP', "2019-03-16 23:59:59");
+    (123, "Underwater Basket Weaver", "Must lift.", "WASHINGTON", "SERVICES", "INTERNSHIP", "2018-07-10 02:30:00"),
+    (456, "Frontend Developer", "Must know everything about React.js.", "NEWYORK", "TECHNOLOGY", "EXECUTIVE", "2019-02-12 12:00:00"),
+    (789, "Backend Developer", "Must know nothing about React.js.", "NEWYORK", "TECHNOLOGY", "INTERNSHIP", "2019-03-16 23:59:59");
 
 CREATE TABLE applications (applicationId INT AUTO_INCREMENT PRIMARY KEY,
                            postingId INT NOT NULL,  -- FOREIGN KEY(postingId), REFERENCES postings (postingId),
