ddb3dae79d870faa19f4975b74f5cd65b3ea87db
Luka Lipovac <lipovac@cooper.edu>
2019-03-23 16:36:56 -0400

    BLD: building frontend. (#1)
    * Added some navbar frontend components
    
    * Added some navbar frontend components
    
    * Added Login and Signup Pages and a beggining to the splash page, but CSS makes no sense so im still working on it.
    
    * Added axios posting for signup, redirection for links on login and signup pages, form checking and submitting
    
    * MAINT: renames

---
+++ euphoria-frontend/src/views/Dashboard.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+class Dashboard extends React.Component {
+  render() {
+    return <h1>Users</h1>
+  }
+}
+export default Dashboard
---
+++ euphoria-frontend/src/views/Description.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+class Description extends React.Component {
+  render() {
+    return <h1>Users</h1>
+  }
+}
+export default Description
