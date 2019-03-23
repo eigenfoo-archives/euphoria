2b44c47b7b0fb82c66ce3207e5399a5b426c36c0
lipovac <lipovac@cooper.com>
2019-03-23 17:49:24 -0400

    REV: delete all the things

--- euphoria-frontend/src/views/Apply.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-class Apply extends React.Component {
-  render() {
-    return <h1>Users</h1>
-  }
-}
-export default Apply
--- euphoria-frontend/src/views/Listings.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-class Listings extends React.Component {
-  render() {
-    return <h1>Users</h1>
-  }
-}
-export default Listings
