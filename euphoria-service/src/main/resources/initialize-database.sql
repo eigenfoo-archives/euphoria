da6773d637d8a01a60ed43e959580756912cb24d
Wendy Ide <ide.s.wendy@gmail.com>
2019-04-15 13:52:09 -0400

    Fileupload (#43)
    * began process to make fileupload to computer filesystem, to avoid hex dumping in database
    
    * file exchange still in Base64, but files saved on filesystem now instead of in DB
    
    * deletes resume and coverletters from associated applications  when a posting is deleted

--- euphoria-service/src/main/resources/initialize-database.sql
+++ euphoria-service/src/main/resources/initialize-database.sql
@@ -86,25 +86,23 @@ CREATE TABLE postings (postingId INT AUTO_INCREMENT PRIMARY KEY,
                        dateCreated TIMESTAMP DEFAULT CURRENT_TIMESTAMP);
 
 INSERT INTO postings
-    (companyId, jobTitle, description, location, industry, skillLevel, dateCreated)
+    (companyId, jobTitle, description, location, industry, skillLevel)
 VALUES
-    (123, "Underwater Basket Weaver", "Must lift.", "WASHINGTON", "SERVICES", "INTERNSHIP", "2018-07-10 02:30:00"),
-    (456, "Frontend Developer", "Must know everything about React.js.", "NEWYORK", "TECHNOLOGY", "EXECUTIVE", "2019-02-12 12:00:00"),
-    (789, "Backend Developer", "Must know nothing about React.js.", "NEWYORK", "TECHNOLOGY", "INTERNSHIP", "2019-03-16 23:59:59");
+    (123, "Underwater Basket Weaver", "Must lift.", "WASHINGTON", "SERVICES", "INTERNSHIP"),
+    (456, "Frontend Developer", "Must know everything about React.js.", "NEWYORK", "TECHNOLOGY", "EXECUTIVE"),
+    (789, "Backend Developer", "Must know nothing about React.js.", "NEWYORK", "TECHNOLOGY", "INTERNSHIP");
 
 CREATE TABLE applications (applicationId INT AUTO_INCREMENT PRIMARY KEY,
                            postingId INT NOT NULL,  -- FOREIGN KEY(postingId), REFERENCES postings (postingId),
                            userId INT NOT NULL, -- FOREIGN KEY(userId), REFERENCES users (userId),
-                           resume LONGBLOB NOT NULL,
-                           coverLetter LONGBLOB NOT NULL,
                            dateCreated TIMESTAMP DEFAULT CURRENT_TIMESTAMP);
 
 INSERT INTO applications
-    (postingId, userId, resume, coverLetter, dateCreated)
+    (postingId, userId)
 VALUES
-	(1, 1, x'54686973', x'553d7a34', "2018-07-11 05:30:00"),
-	(1, 2, x'453d7a38', x'453d7a39', "2018-07-13 03:22:00"),
-	(3, 3, x'653d7a38', x'653e7a39', "2019-02-13 11:40:33");
+	(1, 1),
+	(1, 2),
+	(3, 3);
 
 
 CREATE TABLE authentications (id INT NOT NULL,
