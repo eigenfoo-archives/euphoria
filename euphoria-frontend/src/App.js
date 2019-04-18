ff258f3ac54bc21649101acfaae0a89e0c1b0b48
Luka Lipovac <luka.lipovac@gmail.com>
2019-04-17 23:46:36 -0400

    More cookies (#67)
    * Fixed issues with cookies being passed between components
    
    * Fixed company authentication creation bug. Made cookies work for loading company specific dashboard

--- euphoria-frontend/src/App.js
+++ euphoria-frontend/src/App.js
@@ -26,14 +26,14 @@ class App extends Component {
       <div>
         <Switch>
           <Route exact path="/" component={Splash} />
-          <Route path="/signin" render={(props) => (<Signin {...props} />)} />
+          <Route path="/signin" render={(props) => (<Signin cookies={this.props.cookies} {...props} />)} />
           <Route path="/signup" component={Signup} />
-          <Route exact path="/postings" render={(props) => (<Postings {...props} />)} />
-          <Route path="/postings/apply/:postingId" render={(props) => (<Apply {...props} />)} />
-          <Route exact path="/dashboard" render={(props) => (<Dashboard {...props} />)} />
-          <Route exact path="/dashboard/post" render={(props) => (<Post {...props} />)} />
-          <Route path="/dashboard/post/edit/:postingId" render={(props) => (<Editpost {...props} />)} />
-          <Route path="/dashboard/applications/:postingId" render={(props) => (<Applications {...props} />)} />
+          <Route exact path="/postings" render={(props) => (<Postings cookies={this.props.cookies} {...props} />)} />
+          <Route path="/postings/apply/:postingId" render={(props) => (<Apply cookies={this.props.cookies} {...props} />)} />
+          <Route exact path="/dashboard" render={(props) => (<Dashboard cookies={this.props.cookies} {...props} />)} />
+          <Route exact path="/dashboard/post" render={(props) => (<Post cookies={this.props.cookies} {...props} />)} />
+          <Route path="/dashboard/post/edit/:postingId" render={(props) => (<Editpost cookies={this.props.cookies} {...props} />)} />
+          <Route path="/dashboard/applications/:postingId" render={(props) => (<Applications cookies={this.props.cookies} {...props} />)} />
           <Route path="/404" component={NotFound} />
           <Redirect to="/404" />
         </Switch>
