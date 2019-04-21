34695130c692d72f4adac94139ab6113e8ec43be
George Ho <19851673+eigenfoo@users.noreply.github.com>
2019-04-21 17:02:32 -0400

    TST: user tests done

--- euphoria-service/src/test/java/edu/cooper/ece366/euphoria/handler/UserHandlersTest.java
+++ euphoria-service/src/test/java/edu/cooper/ece366/euphoria/handler/UserHandlersTest.java
@@ -3,7 +3,11 @@ package edu.cooper.ece366.euphoria;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.spotify.apollo.Request;
 import com.spotify.apollo.RequestContext;
-import com.typesafe.config.Config;
+import edu.cooper.ece366.euphoria.handler.UserHandlers;
+import edu.cooper.ece366.euphoria.model.User;
+import edu.cooper.ece366.euphoria.model.UserBuilder;
+import edu.cooper.ece366.euphoria.store.model.UserStore;
+import edu.cooper.ece366.euphoria.utils.EducationLevel;
 import okio.ByteString;
 import org.junit.Before;
 import org.junit.Test;
@@ -12,20 +16,23 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class UserHandlersTest {
-
     @Mock
     ObjectMapper objectMapper;
     @Mock
-    Config config;
+    UserStore userStore;
     @Mock
-    RequestContext rc;
+    RequestContext requestContext;
     @Mock
     Request request;
     @Mock
@@ -35,7 +42,7 @@ public class UserHandlersTest {
 
     @Before
     public void setup() {
-        testClass = new UserHandlers(objectMapper, config);
+        testClass = new UserHandlers(objectMapper, userStore);
     }
 
     @Test
@@ -51,22 +58,22 @@ public class UserHandlersTest {
                 .build();
 
         // Mock dependencies and inputs
-        when(config.getString("mysql.jdbc")).thenReturn("jdbc:mysql://localhost:3306/euphoria");
-        when(config.getString("mysql.user")).thenReturn("euphoria");
-        when(config.getString("mysql.password")).thenReturn("euphoria");
-        when(rc.pathArgs()).thenReturn(Collections.singletonMap("userId", "1"));
+        when(requestContext.pathArgs()).thenReturn(Collections.singletonMap("userId", "1"));
+        when(userStore.getUser("1")).thenReturn(expected);
 
         // Call test class
-        User actual = testClass.getUser(rc).get(0);
+        User actual = testClass.getUser(requestContext);
 
         // Assert and verify
         assertEquals(expected, actual);
+
+        verifyZeroInteractions(objectMapper);
     }
 
     @Test
     public void createUser() throws IOException {
         // Setup variables
-        User user = new UserBuilder()
+        User expected = new UserBuilder()
                 .userId(4)
                 .name("NA")
                 .email("NA")
@@ -74,7 +81,6 @@ public class UserHandlersTest {
                 .educationLevel(EducationLevel.NOHIGHSCHOOL)
                 .description("NA")
                 .build();
-        List<User> expected = Collections.singletonList(user);
         byte[] byteArray = new byte[0];
 
         Map<String, Object> map = new HashMap<>();
@@ -85,16 +91,14 @@ public class UserHandlersTest {
         map.put("educationLevel", "BACHELORS");
         map.put("description", "Am engineer pls hire.");
 
-        when(config.getString("mysql.jdbc")).thenReturn("jdbc:mysql://localhost:3306/euphoria");
-        when(config.getString("mysql.user")).thenReturn("euphoria");
-        when(config.getString("mysql.password")).thenReturn("euphoria");
-        when(rc.request()).thenReturn(request);
+        when(requestContext.request()).thenReturn(request);
         when(request.payload()).thenReturn(Optional.of(requestPayloadByteString));
         when(requestPayloadByteString.toByteArray()).thenReturn(byteArray);
         when(objectMapper.readValue(byteArray, Map.class)).thenReturn(map);
+        when(userStore.createUser("John Smith", "john@smith.com", "123-456-7890", EducationLevel.BACHELORS, "Am engineer pls hire.")).thenReturn(expected);
 
         // Call test class
-        List<User> actual = testClass.createUser(rc);
+        User actual = testClass.createUser(requestContext);
 
         // Assert and verify
         assertEquals(expected, actual);
