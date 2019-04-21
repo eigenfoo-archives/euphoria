28629e26eceb9bdd4c31204a7543465c7a670f1a
Wendy Ide <ide.s.wendy@gmail.com>
2019-04-21 00:06:11 -0400

    got applications working with store layer, tested

--- euphoria-service/src/main/java/edu/cooper/ece366/euphoria/handler/ApplicationHandlers.java
+++ euphoria-service/src/main/java/edu/cooper/ece366/euphoria/handler/ApplicationHandlers.java
@@ -1,209 +1,70 @@
 package edu.cooper.ece366.euphoria.handler;
 
-import edu.cooper.ece366.euphoria.model.*;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.annotations.VisibleForTesting;
 import com.spotify.apollo.RequestContext;
 import com.spotify.apollo.Response;
 import com.spotify.apollo.route.*;
-import com.typesafe.config.Config;
+import edu.cooper.ece366.euphoria.model.Application;
+import edu.cooper.ece366.euphoria.store.model.ApplicationStore;
 import okio.ByteString;
-import java.util.Base64;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.sql.*;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Stream;
-import java.util.ArrayList;
 
 public class ApplicationHandlers implements RouteProvider {
     private final ObjectMapper objectMapper;
-    private final Config config;
-    private final String FileStoragePath;
+    private final ApplicationStore applicationStore;
 
-    public ApplicationHandlers(final ObjectMapper objectMapper, final Config config) {
+    public ApplicationHandlers(final ObjectMapper objectMapper, ApplicationStore applicationStore) {
         this.objectMapper = objectMapper;
-        this.config = config;
-        FileStoragePath = config.getString("FileStoragePath");
+        this.applicationStore = applicationStore;
     }
 
     @Override
     public Stream<Route<AsyncHandler<Response<ByteString>>>> routes() {
         return Stream.of(
-                Route.sync("GET", "/api/application/<applicationId>", this::getApplication),
-                Route.sync("GET", "/api/application/posting/<postingId>", this::getApplicationsForPosting),
-                Route.sync("POST", "/api/application", this::createApplication)
-        ).map(r -> r.withMiddleware(jsonMiddleware()));
+                Route.sync("GET", "/api/application/<applicationId>", this::getApplication).withMiddleware(jsonMiddleware()),
+                Route.sync("GET", "/api/application/posting/<postingId>", this::getApplicationsForPosting).withMiddleware(jsonMiddleware()),
+                Route.sync("POST", "/api/application", this::createApplication).withMiddleware(jsonMiddleware())
+        );
     }
 
     @VisibleForTesting
-    public List<Application> getApplication(final RequestContext rc) {
-        Application application = null;
-
-        Integer applicationId = Integer.valueOf(rc.pathArgs().get("applicationId"));
-
-        File fileRes = new File(FileStoragePath + "app_" + applicationId + "/resume_" + applicationId + ".pdf");
-        byte[] bufferRes = new byte[(int) fileRes.length()];
-        File fileCov = new File(FileStoragePath + "app_" + applicationId + "/cover_" + applicationId + ".pdf");
-        byte[] bufferCov = new byte[(int) fileCov.length()];
-            try {
-                FileInputStream input1 = new FileInputStream(fileRes);
-                input1.read(bufferRes);
-                input1.close();
-                FileInputStream input2 = new FileInputStream(fileCov);
-                input2.read(bufferCov);
-                input2.close();
-            } catch (IOException e) {
-                System.out.println(e.getMessage());
-            }
-
-        try {
-            Connection conn = DriverManager.getConnection(
-                    config.getString("mysql.jdbc"),
-                    config.getString("mysql.user"),
-                    config.getString("mysql.password"));
-            String sqlQuery = "SELECT * FROM applications WHERE applicationId = ?";
-            PreparedStatement ps = conn.prepareStatement(sqlQuery);
-            ps.setInt(1, applicationId);
-            ResultSet rs = ps.executeQuery();
-
-            if (rs.first()) {
-                application = new ApplicationBuilder()
-                        .applicationId(rs.getInt("applicationId"))
-                        .postingId(rs.getInt("postingId"))
-                        .userId(rs.getInt("userId"))
-                        .resume(bufferRes)        //returns BASE64
-                        .coverLetter(bufferCov)   //returns BASE64
-                        .dateCreated(rs.getString("dateCreated"))
-                        .build();
-            }
-        } catch (SQLException ex) {
-            System.out.println(ex);
-        }
-
-        return Collections.singletonList(application);
+    Application getApplication(final RequestContext rc) {
+        return applicationStore.getApplication(rc.pathArgs().get("applicationId"));
     }
 
     @VisibleForTesting
-    public List<Application> getApplicationsForPosting(final RequestContext rc) {
-        ArrayList<Application> applicationList = new ArrayList<Application>();
-
-        try {
-            Integer postingId = Integer.valueOf(rc.pathArgs().get("postingId"));
-            Connection conn = DriverManager.getConnection(
-                    config.getString("mysql.jdbc"),
-                    config.getString("mysql.user"),
-                    config.getString("mysql.password"));
-            String sqlQuery = "SELECT * FROM applications WHERE postingId = ?";
-            PreparedStatement ps = conn.prepareStatement(sqlQuery);
-            ps.setInt(1, postingId);
-            ResultSet rs = ps.executeQuery();
-
-            while (rs.next()) {
-                Integer applicationId = rs.getInt("applicationId");
-                File fileRes = new File(FileStoragePath + "app_" + applicationId + "/resume_" + applicationId + ".pdf");
-                byte[] bufferRes = new byte[(int) fileRes.length()];
-                File fileCov = new File(FileStoragePath + "app_" + applicationId + "/cover_" + applicationId + ".pdf");
-                byte[] bufferCov = new byte[(int) fileCov.length()];
-                try {
-                    FileInputStream input1 = new FileInputStream(fileRes);
-                    input1.read(bufferRes);
-                    input1.close();
-                    FileInputStream input2 = new FileInputStream(fileCov);
-                    input2.read(bufferCov);
-                    input2.close();
-                } catch (IOException e) {
-                    System.out.println(e.getMessage());
-                }
-
-                Application application = new ApplicationBuilder()
-                        .applicationId(applicationId)
-                        .postingId(rs.getInt("postingId"))
-                        .userId(rs.getInt("userId"))
-                        .resume(bufferRes)        //returns BASE64
-                        .coverLetter(bufferCov)   //returns BASE64
-                        .dateCreated(rs.getString("dateCreated"))
-                        .build();
-
-                applicationList.add(application);
-            }
-        } catch (SQLException ex) {
-            System.out.println(ex);
-        }
-
-        return applicationList;
+    List<Application> getApplicationsForPosting(final RequestContext rc) {
+        return applicationStore.getApplicationsForPosting(rc.pathArgs().get("postingId"));
     }
 
     @VisibleForTesting
-    public List<Application> createApplication(final RequestContext rc) {
-        Integer applicationId;
+    List<Application> createApplication(final RequestContext rc) {
+        String postingId = null;
+        String userId = null;
+        String resume = null;
+        String coverLetter = null;
+        boolean success = false;
         try {
             byte[] requestBytes = rc.request().payload().get().toByteArray();
             Map jsonMap = objectMapper.readValue(requestBytes, Map.class);
-            Integer postingId = Integer.valueOf(jsonMap.get("postingId").toString());
-            Integer userId = Integer.valueOf(jsonMap.get("userId").toString());
-            String resume = jsonMap.get("resume").toString();
-            String coverLetter = jsonMap.get("coverLetter").toString();
-
-            Connection conn = DriverManager.getConnection(
-                    config.getString("mysql.jdbc"),
-                    config.getString("mysql.user"),
-                    config.getString("mysql.password"));
-            String sqlQuery = "INSERT INTO applications (postingId, userId) " +
-                              "VALUES (?, ?)";
-            PreparedStatement ps = conn.prepareStatement(sqlQuery, Statement.RETURN_GENERATED_KEYS);
-            ps.setInt(1, postingId);
-            ps.setInt(2, userId);
-            //timestamped automatically in UTC by mysql database
-            int rowsAffected = ps.executeUpdate();
-
-            //get ApplicationId
-            if (rowsAffected == 0) {
-                throw new SQLException("Creating new application failed, no rows affected.");
-            }
-            try (ResultSet generatedKeys = ps.getGeneratedKeys()) {
-                if (generatedKeys.next()) {
-                     applicationId = generatedKeys.getInt(1);
-                } else {
-                    throw new SQLException("Creating new application failed, no ID obtained.");
-                }
-            }
-            //write to file system
-            byte[] decodedRes = Base64.getDecoder().decode(resume);
-            byte[] decodedCov = Base64.getDecoder().decode(coverLetter);
-
-            try {
-                File newDir = new File(FileStoragePath + "app_" + applicationId);
-                newDir.mkdir();
-                FileOutputStream output1 = new FileOutputStream(FileStoragePath + "app_" + applicationId + "/resume_" + applicationId + ".pdf");
-                output1.write(decodedRes);
-                FileOutputStream output2 = new FileOutputStream(FileStoragePath + "app_" + applicationId + "/cover_" + applicationId + ".pdf");
-                output2.write(decodedCov);
-            } catch (IOException e) {
-                System.out.println(e.getMessage());
-            }
-
-            sqlQuery = "UPDATE applications SET resumeLocation = ?,  coverLetterLocation = ? WHERE applicationId = ? ";
-            ps = conn.prepareStatement(sqlQuery, Statement.RETURN_GENERATED_KEYS);
-            ps.setString(1, "file://" + FileStoragePath + "app_" + applicationId + "/resume_" + applicationId + ".pdf");
-            ps.setString(2, "file://" + FileStoragePath + "app_" + applicationId + "/cover_" + applicationId + ".pdf");
-            ps.setInt(3, applicationId);
-            rowsAffected = ps.executeUpdate();
-            if (rowsAffected == 0) {
-                throw new SQLException("Inserting application file paths failed, no rows affected.");
-            }
-
-        } catch (SQLException | IOException ex) {
+            postingId = jsonMap.get("postingId").toString();
+            userId = jsonMap.get("userId").toString();
+            resume = jsonMap.get("resume").toString();
+            coverLetter = jsonMap.get("coverLetter").toString();
+            success = true;
+        } catch (IOException ex) {
             System.out.println(ex);
         }
 
-        return Collections.emptyList();
+        if (success)
+            return applicationStore.createApplication(postingId, userId, resume, coverLetter);
+        else
+            return null;
     }
 
     private <T> Middleware<AsyncHandler<T>, AsyncHandler<Response<ByteString>>> jsonMiddleware() {
---
+++ euphoria-service/src/main/java/edu/cooper/ece366/euphoria/store/jdbc/ApplicationStoreJdbc.java
@@ -0,0 +1,191 @@
+package edu.cooper.ece366.euphoria.store.jdbc;
+
+import com.google.common.annotations.VisibleForTesting;
+import com.spotify.apollo.RequestContext;
+import com.typesafe.config.Config;
+import edu.cooper.ece366.euphoria.model.*;
+import edu.cooper.ece366.euphoria.store.model.ApplicationStore;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.sql.*;
+import java.util.*;
+
+public class ApplicationStoreJdbc implements ApplicationStore {
+
+    private static final String GET_APPLICATION_STATEMENT = "SELECT * FROM applications WHERE applicationId = ?";
+    private static final String GET_APPLICATIONS_FOR_POSTING_STATEMENT = "SELECT * FROM applications WHERE postingId = ?";
+    private static final String CREATE_APPLICATION_STATEMENT  = "INSERT INTO applications (postingId, userId) VALUES (?, ?)";
+    private static final String ADD_FILE_LOCATIONS_STATEMENT =  "UPDATE applications SET resumeLocation = ?,  coverLetterLocation = ? WHERE applicationId = ? ";
+
+    private final Config config;
+    private final String FileStoragePath;
+
+    public ApplicationStoreJdbc(final Config config) {
+        this.config = config;
+        FileStoragePath = config.getString("FileStoragePath");
+    }
+
+    @Override
+    public Application getApplication(final String applicationId) {
+
+        File fileRes = new File(FileStoragePath + "app_" + applicationId + "/resume_" + applicationId + ".pdf");
+        byte[] bufferRes = new byte[(int) fileRes.length()];
+        File fileCov = new File(FileStoragePath + "app_" + applicationId + "/cover_" + applicationId + ".pdf");
+        byte[] bufferCov = new byte[(int) fileCov.length()];
+        try {
+            FileInputStream input1 = new FileInputStream(fileRes);
+            input1.read(bufferRes);
+            input1.close();
+            FileInputStream input2 = new FileInputStream(fileCov);
+            input2.read(bufferCov);
+            input2.close();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        Connection connection;
+        try {
+            connection =
+                    DriverManager.getConnection(
+                            config.getString("mysql.jdbc"),
+                            config.getString("mysql.user"),
+                            config.getString("mysql.password"));
+
+            PreparedStatement ps = connection.prepareStatement(GET_APPLICATION_STATEMENT);
+            ps.setInt(1, Integer.parseInt(applicationId));
+
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.first()) {
+                return new ApplicationBuilder()
+                        .applicationId(rs.getInt("applicationId"))
+                        .postingId(rs.getInt("postingId"))
+                        .userId(rs.getInt("userId"))
+                        .resume(bufferRes)        //returns BASE64
+                        .coverLetter(bufferCov)   //returns BASE64
+                        .dateCreated(rs.getString("dateCreated"))
+                        .build();
+            } else {
+                return null;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("error fetching application", e);
+        }
+    }
+
+    @Override
+    public List<Application> getApplicationsForPosting(final String postingId) {
+        ArrayList<Application> applicationList = new ArrayList<Application>();
+
+        Connection connection;
+        try {
+            connection =
+                    DriverManager.getConnection(
+                            config.getString("mysql.jdbc"),
+                            config.getString("mysql.user"),
+                            config.getString("mysql.password"));
+
+            PreparedStatement ps = connection.prepareStatement(GET_APPLICATIONS_FOR_POSTING_STATEMENT);
+            ps.setInt(1, Integer.parseInt(postingId));
+
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                Integer applicationId = rs.getInt("applicationId");
+                File fileRes = new File(FileStoragePath + "app_" + applicationId + "/resume_" + applicationId + ".pdf");
+                byte[] bufferRes = new byte[(int) fileRes.length()];
+                File fileCov = new File(FileStoragePath + "app_" + applicationId + "/cover_" + applicationId + ".pdf");
+                byte[] bufferCov = new byte[(int) fileCov.length()];
+                try {
+                    FileInputStream input1 = new FileInputStream(fileRes);
+                    input1.read(bufferRes);
+                    input1.close();
+                    FileInputStream input2 = new FileInputStream(fileCov);
+                    input2.read(bufferCov);
+                    input2.close();
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                }
+
+                Application application = new ApplicationBuilder()
+                        .applicationId(applicationId)
+                        .postingId(rs.getInt("postingId"))
+                        .userId(rs.getInt("userId"))
+                        .resume(bufferRes)        //returns BASE64
+                        .coverLetter(bufferCov)   //returns BASE64
+                        .dateCreated(rs.getString("dateCreated"))
+                        .build();
+
+                applicationList.add(application);
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+
+        return applicationList;
+    }
+
+    @Override
+    public List<Application> createApplication(final String postingId, final String userId, final String resume, final String coverLetter) {
+        Integer applicationId;
+        Connection connection;
+        try {
+            connection =
+                    DriverManager.getConnection(
+                            config.getString("mysql.jdbc"),
+                            config.getString("mysql.user"),
+                            config.getString("mysql.password"));
+            PreparedStatement ps = connection.prepareStatement(CREATE_APPLICATION_STATEMENT, Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, Integer.parseInt(postingId));
+            ps.setInt(2, Integer.parseInt(userId));
+            //timestamped automatically in UTC by mysql database
+            int rowsAffected = ps.executeUpdate();
+
+            //get ApplicationId
+            if (rowsAffected == 0) {
+                throw new SQLException("Creating new application failed, no rows affected.");
+            }
+            try (ResultSet generatedKeys = ps.getGeneratedKeys()) {
+                if (generatedKeys.next()) {
+                    applicationId = generatedKeys.getInt(1);
+                } else {
+                    throw new SQLException("Creating new application failed, no ID obtained.");
+                }
+            }
+            //write to file system
+            byte[] decodedRes = Base64.getDecoder().decode(resume);
+            byte[] decodedCov = Base64.getDecoder().decode(coverLetter);
+
+            try {
+                File newDir = new File(FileStoragePath + "app_" + applicationId);
+                newDir.mkdir();
+                FileOutputStream output1 = new FileOutputStream(FileStoragePath + "app_" + applicationId + "/resume_" + applicationId + ".pdf");
+                output1.write(decodedRes);
+                FileOutputStream output2 = new FileOutputStream(FileStoragePath + "app_" + applicationId + "/cover_" + applicationId + ".pdf");
+                output2.write(decodedCov);
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+
+            ps = connection.prepareStatement(ADD_FILE_LOCATIONS_STATEMENT);
+            ps.setString(1, "file://" + FileStoragePath + "app_" + applicationId + "/resume_" + applicationId + ".pdf");
+            ps.setString(2, "file://" + FileStoragePath + "app_" + applicationId + "/cover_" + applicationId + ".pdf");
+            ps.setInt(3, applicationId);
+            rowsAffected = ps.executeUpdate();
+            if (rowsAffected == 0) {
+                throw new SQLException("Inserting application file paths failed, no rows affected.");
+            }
+
+            return Collections.emptyList(); //if everything successful, return empty list
+
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+
+        return null; //if not successful, return null
+    }
+
+}
