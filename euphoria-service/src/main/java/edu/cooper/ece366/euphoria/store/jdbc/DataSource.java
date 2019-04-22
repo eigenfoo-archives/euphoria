b76643c7587a2815c3a81e77ef8e26b6fdd85daa
George Ho <19851673+eigenfoo@users.noreply.github.com>
2019-04-22 00:00:50 -0400

    BLD: make basic data source class

---
+++ euphoria-service/src/main/java/edu/cooper/ece366/euphoria/store/jdbc/DataSource.java
@@ -0,0 +1,22 @@
+package edu.cooper.ece366.euphoria.store.jdbc;
+
+import com.typesafe.config.Config;
+import org.apache.commons.dbcp.BasicDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class DataSource {
+    private static final BasicDataSource dataSource = new BasicDataSource();
+
+    private DataSource(final Config config) {
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUsername(config.getString("mysql.user"));
+        dataSource.setPassword(config.getString("mysql.password")))
+        dataSource.setUrl(config.getString("mysql.jdbc"));
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return dataSource.getConnection();
+    }
+}
